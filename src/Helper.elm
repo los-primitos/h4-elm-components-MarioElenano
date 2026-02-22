e0bc19463e4b020240e7addef625ae1dc79c9df3
MariaElenaLinares <up230806@alumnos.upa.edu.mx>
2026-02-22 13:44:26 -0600

    Update Helper.elm

--- src/Helper.elm
+++ src/Helper.elm
@@ -9,3 +9,35 @@ import Html.Attributes
 --main : Html.Html msg
 --main =
 --    Html.div [] [ headers "Titulos", hyperlink "https://upa.edu.mx" "My School" ]
+
+
+joinWords : String -> String -> String
+joinWords word1 word2 =
+    word1 ++ word2
+
+
+isUpperChars : List Char -> List Bool
+isUpperChars list =
+    List.map Char.isUpper list
+
+
+evalChars : List Char -> (Char -> Bool) -> List Bool
+evalChars list funTrans =
+    List.map funTrans list
+
+
+headers : String -> Html.Html msg
+headers param =
+    Html.div []
+        [ Html.h1 [] [ Html.text param ]
+        , Html.h2 [] [ Html.text param ]
+        , Html.h3 [] [ Html.text param ]
+        , Html.h4 [] [ Html.text param ]
+        , Html.h5 [] [ Html.text param ]
+        , Html.h6 [] [ Html.text param ]
+        ]
+
+
+hyperlink : String -> String -> Html.Html msg
+hyperlink url label =
+    Html.a [ Html.Attributes.href url ] [ Html.text label ]
